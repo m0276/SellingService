7781977e519fdff09b6eb313c1f568f91c58e25d
LeeMinJu <mmnnii0276@gmail.com>
2025-04-14 18:16:41 +0900

    feat: add sort list method

--- src/main/java/MJ/sellingservice/dto/SellingDto.java
+++ src/main/java/MJ/sellingservice/dto/SellingDto.java
@@ -5,32 +5,43 @@ import lombok.Data;
 
 @Data
 public class SellingDto {
-  String auctn_seq;
-  String corp_cd;
+  String corp_cd; // 법인
   String corp_gds_cd;
   String corp_gds_item_nm;
   String corp_gds_vrty_nm;
   String corp_nm;
-  String gds_lclsf_cd;
+  String gds_lclsf_cd; // 품목 코드
   String gds_lclsf_nm;
   String gds_mclsf_cd;
   String gds_mclsf_nm;
   String gds_sclsf_cd;
   String gds_sclsf_nm;
-  String pkg_cd;
+  String pkg_cd; // 포장 코드
   String pkg_nm;
-  String plor_cd;
+  String plor_cd; // 산지 코드
   String plor_nm;
-  String qty;
-  String scsbd_dt;
-  String scsbd_prc;
-  String spm_no;
-  String trd_clcln_ymd;
-  String trd_se;
-  String unit_cd;
+  String qty; // 총 수량
+  String scsbd_dt; // 낙찰일
+  String scsbd_prc; // 낙찰가
+  String spm_no; // 상품 관리 번호
+  String trd_clcln_ymd; // 정산일
+  String trd_se; //거래 유형
+  String unit_cd; // 단위
   String unit_nm;
-  String unit_qty;
-  String whsl_mrkt_cd;
+  String unit_qty; // 한 단위 당 수량
+  String whsl_mrkt_cd; // 도매시장
   String whsl_mrkt_nm;
 
 }
+
+
+//whsl_mrkt_cd : 도매시장 코드
+//corp_cd : 법인 코드
+//unit_cd : 단위 코드
+//sz_cd : 크기 코드
+//plor_cd : 산지 코드
+//pkg_cd : 포장 코드
+//grd_cd : 등급 코드
+//gds_lclsf_cd : 품목 코드 (대분류)
+//gds_mclsf_cd : 품목 코드 (중분류)
+//gds_sclsf_cd : 품목 코드 (소분류)
--- src/main/java/MJ/sellingservice/service/SellingAPIService.java
+++ src/main/java/MJ/sellingservice/service/SellingAPIService.java
@@ -3,11 +3,19 @@ package MJ.sellingservice.service;
 
 import MJ.sellingservice.dto.SellingDto;
 import MJ.sellingservice.exception.NoCorrectUrlException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -27,6 +35,10 @@ public class SellingAPIService {
   private String serviceKey;
 
   private final RestTemplate restTemplate = new RestTemplate();
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private Map<String,String> market = new HashMap<>();
+  private Map<String,String> product = new HashMap<>();
 
   public List<SellingDto> getAllList(){
     StringBuilder sb = new StringBuilder(url);
@@ -34,18 +46,52 @@ public class SellingAPIService {
     try{
       URI requestUrl = new URI(sb.toString());
 
-      ResponseEntity<List<SellingDto>> response =
-          restTemplate.exchange(requestUrl,
-              HttpMethod.GET,
-              null,
-              new ParameterizedTypeReference<List<SellingDto>>() {});
+      ResponseEntity<String> response = restTemplate.getForEntity(requestUrl, String.class);
 
-      return response.getBody();
+      JsonNode root = objectMapper.readTree(response.getBody());
+
+      JsonNode itemArray = root
+          .path("response")
+          .path("body")
+          .path("items")
+          .path("item");
+
+      List<SellingDto> items = new ArrayList<>();
+
+      if (itemArray.isArray()) {
+        for (JsonNode itemNode : itemArray) {
+          SellingDto dto = objectMapper.treeToValue(itemNode, SellingDto.class);
+          market.put(dto.getWhsl_mrkt_nm(), dto.getWhsl_mrkt_cd());
+          product.put(dto.getGds_mclsf_nm(),dto.getGds_mclsf_nm());
+          items.add(dto);
+        }
+      }
+
+      return items;
 
     } catch (URISyntaxException e) {
       throw new NoCorrectUrlException("해당 경로가 정확하지 않습니다. 다시 확인 후 시도해 주세요.");
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
     }
-
   }
 
+  // 공판장 별 날짜로 정렬
+  // TODO : 생각해 볼 것 -> 거리 별로 하는 것은 어떠할지?
+  public List<SellingDto> sortByMarket(){
+    List<SellingDto> list = getAllList();
+    list.sort(Comparator.comparing(SellingDto::getWhsl_mrkt_cd)
+        .thenComparing(SellingDto::getScsbd_dt));
+
+    return list;
+  }
+
+  // 물품 중분류와 소분류 별 정렬
+  public List<SellingDto> sortByProduct(){
+    List<SellingDto> list = getAllList();
+    list.sort(Comparator.comparing(SellingDto::getGds_mclsf_cd)
+        .thenComparing(SellingDto::getGds_sclsf_cd));
+
+    return list;
+  }
 }

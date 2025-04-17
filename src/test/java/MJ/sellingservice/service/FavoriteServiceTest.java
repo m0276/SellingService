7499e3f5e34767462a633f9f7db321c944157f65
LeeMinJu <mmnnii0276@gmail.com>
2025-04-17 14:34:44 +0900

    test: add favorite service test

---
+++ src/test/java/MJ/sellingservice/service/FavoriteServiceTest.java
@@ -0,0 +1,75 @@
+package MJ.sellingservice.service;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import MJ.sellingservice.domain.Favorite;
+import MJ.sellingservice.domain.User;
+import MJ.sellingservice.dto.FavoriteDto;
+import MJ.sellingservice.mapper.UserMapper;
+import MJ.sellingservice.repository.FavoriteRepository;
+import MJ.sellingservice.util.LoginUtil;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.test.context.support.WithMockUser;
+
+@ExtendWith(MockitoExtension.class)
+class FavoriteServiceTest {
+  @Mock
+  private FavoriteRepository favoriteRepository;
+
+  @Mock
+  private UserService userService;
+
+  @Mock
+  private UserMapper userMapper;
+
+  @InjectMocks
+  private FavoriteService favoriteService; // 진짜 객체 + mock 주입됨
+
+
+
+  @Test
+  void get() {
+
+    try (MockedStatic<LoginUtil> mockedLoginUtil = mockStatic(LoginUtil.class)) {
+      //when(favoriteRepository.findAll()).thenReturn(Collections.emptyList());
+      mockedLoginUtil.when(LoginUtil::isLogin).thenReturn(true);
+      mockedLoginUtil.when(LoginUtil::getCurrentUserEmail).thenReturn("test@example.com");
+
+      List<FavoriteDto> list =  favoriteService.get();
+
+      assertTrue(list.isEmpty(), "empty List");
+    }
+  }
+
+  @Test
+  void checkAndSave_shouldSave() {
+    try (MockedStatic<LoginUtil> mockedLoginUtil = mockStatic(LoginUtil.class)) {
+      mockedLoginUtil.when(LoginUtil::isLogin).thenReturn(true);
+      mockedLoginUtil.when(LoginUtil::getCurrentUserEmail).thenReturn("test@example.com");
+
+      User mockUser = mock(User.class);
+      when(userService.findByUserEmail("test@example.com")).thenReturn(mockUser);
+
+      // 테스트 대상
+      FavoriteDto favoriteDto = new FavoriteDto();
+      favoriteDto.setFavorite(true);
+      favoriteService.checkAndSave(favoriteDto);
+
+      verify(favoriteRepository).save(any(Favorite.class));
+      verify(favoriteRepository, never()).delete(any(Favorite.class));
+    }
+  }
+}

4bbc2c3ad60f8176076af9872ff0253b7d3e38e2
LeeMinJu <mmnnii0276@gmail.com>
2025-04-24 15:22:20 +0900

    feat: add filter chain about naver login

--- src/main/java/MJ/sellingservice/config/SecurityConfigure.java
+++ src/main/java/MJ/sellingservice/config/SecurityConfigure.java
@@ -36,6 +36,7 @@ public class SecurityConfigure {
             .requestMatchers("/",
                 "/api/users/join",
                 "/api/infos/**",
+                "/oauth/naver/**",
                 "/css/**",
                 "/js/**",
                 "/images/**",

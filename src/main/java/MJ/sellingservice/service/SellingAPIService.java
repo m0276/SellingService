914e5ff9b876258a21a0e7b9669c3a247dcf653d
LeeMinJu <mmnnii0276@gmail.com>
2025-05-13 13:51:22 +0900

    fix: product, market search error

--- src/main/java/MJ/sellingservice/service/SellingAPIService.java
+++ src/main/java/MJ/sellingservice/service/SellingAPIService.java
@@ -35,7 +35,7 @@ public class SellingAPIService {
   private final ObjectMapper objectMapper = new ObjectMapper();
 
   // 검색용
-  private Map<String,String> market = new HashMap<>();
+
   private Map<String,String> product = new HashMap<>();
 
 
@@ -43,20 +43,14 @@ public class SellingAPIService {
   public List<SellingDto> getAllList(){
     StringBuilder sb = new StringBuilder(url);
     sb.append("?serviceKey=").append(serviceKey)
-        .append("&numOfRows=1000");
+        .append("&numOfRows=10000");
     List<SellingDto> list = makeList(sb);
     list.sort(Comparator.comparing(SellingDto::getScsbd_dt));
-
-    for(SellingDto dto : list){
-      market.put(dto.getWhsl_mrkt_nm(),dto.getWhsl_mrkt_cd());
-      product.put(dto.getGds_mclsf_nm(),dto.getGds_mclsf_cd());
-    }
 
     return list;
   }
 
   // 공판장 별 날짜로 정렬
-  // TODO : 생각해 볼 것 -> 거리 별로 하는 것은 어떠할지?
   public List<SellingDto> sortByMarket(){
     List<SellingDto> list = getAllList();
     list.sort(Comparator.comparing(SellingDto::getWhsl_mrkt_cd)
@@ -78,20 +72,25 @@ public class SellingAPIService {
 
   //공판장으로 검색
   public List<SellingDto> findWithMarket(String marketName){
-    List<SellingDto> result = new ArrayList<>();
+    Map<String,String> market = new HashMap<>();
 
+    for(SellingDto dto : getAllList()) {
+      market.put(dto.getWhsl_mrkt_nm(), dto.getWhsl_mrkt_cd());
+    }
+
+    List<SellingDto> result = new ArrayList<>();
     if(market.keySet().stream().anyMatch(key -> key.matches(".*"+ Pattern.quote(marketName)+".*"))){
       StringBuilder sb = new StringBuilder(url);
       sb.append("?serviceKey=").append(serviceKey)
           .append("&numOfRows=1000")
-          .append("&cond[whsl_mrkt_cd::EQ]=");
+          .append("&cond%5Bwhsl_mrkt_cd%3A%3AEQ%5D=");
 
       List<String> matchingKeys = market.keySet().stream()
           .filter(key -> key.matches(".*" + Pattern.quote(marketName) + ".*"))
           .toList();
       for (String s : matchingKeys){
         StringBuilder str = new StringBuilder(sb);
-        str.append(s);
+        str.append(market.get(s));
         result.addAll(makeList(str));
       }
 
@@ -102,11 +101,17 @@ public class SellingAPIService {
 
   //물품별 검색
   public List<SellingDto> findWithProduct(String productName){
+    Map<String,String> product = new HashMap<>();
+
+    for(SellingDto dto : getAllList()) {
+      product.put(dto.getGds_mclsf_nm(),dto.getGds_mclsf_cd());
+    }
+
     if(product.keySet().stream().anyMatch(key -> key.matches(".*"+ Pattern.quote(productName)+".*"))){
       StringBuilder sb = new StringBuilder(url);
       sb.append("?serviceKey=").append(serviceKey)
           .append("&numOfRows=1000")
-          .append("&cond[gds_mclsf_cd::EQ]=").append(product.get(productName));
+          .append("&cond%5Bgds_mclsf_cd%3A%3AEQ%5D=").append(product.get(productName));
       return makeList(sb);
     }
     else throw new NoCorrectException(productName+"에 해당하는 물품을 찾을 수 없습니다.");
